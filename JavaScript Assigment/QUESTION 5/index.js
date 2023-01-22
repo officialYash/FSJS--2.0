0c6850c3435febda6014475983ef9c3cdd555035
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-22 17:26:06 +0530

    Question 5 done of JavaScript Assigment

---
+++ JavaScript Assigment/QUESTION 5/index.js
@@ -0,0 +1,9 @@
+//05. Declare a varibale and assign string value to it and then split it into an array using split() method
+
+
+const str1 = "javascript is easy to learn . even a child can also Master to it 😅😂";
+const arr = str1.split(" "); //We are splitting the string based on whitespace
+
+//Output this Program
+
+console.log(arr);
