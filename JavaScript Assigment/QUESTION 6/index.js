5468cce672263ed2909c3ef3a89c86219c66ec53
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-22 17:33:10 +0530

    Question 6 done of JavaScript Assigment

---
+++ JavaScript Assigment/QUESTION 6/index.js
@@ -0,0 +1,9 @@
+//06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+const str1 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+
+const arr = str.split(","); // Splitting based on Comma
+
+// Output this Program
+
+console.log(arr);
