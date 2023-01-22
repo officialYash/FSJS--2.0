dce2d5c83630487c7f3e234ea626eabdfa811bd3
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-22 16:41:24 +0530

    Question 4 done of JavaScript Assigment

---
+++ JavaScript Assigment/QUESTION 4/index.js
@@ -0,0 +1,8 @@
+//04. Declare a varibale and assign string value to it and check if the string contains a word Script using includes() method
+
+const str = " to add a JS File in Html  you need to use the Script Tag 😃";
+const ScriptIncludes = str.includes("Script");
+
+// output this program
+
+console.log(ScriptIncludes);
