02e57f90ff95d452527eae3986776f23624608f4
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-22 18:02:56 +0530

    Question 8 done of JavaScript Assigment

---
+++ JavaScript Assigment/QUESTION 8/index.js
@@ -0,0 +1,9 @@
+//Q.8 Demonstrate the use of trim() to remove any trailing whitespace at the beginning and the end of a string.
+
+
+const str1 ="         Yash Arora        ";
+const trueStr = str1.trim(); // trim  method will remove whitespace at beginning and at the end of string
+
+// Output the Program
+
+console.log(trueStr);
