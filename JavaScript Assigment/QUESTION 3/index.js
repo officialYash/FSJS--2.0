c620d121fae9e3eebc0ee6513555ac435e2c814c
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-22 16:33:41 +0530

    Question 3 done of JavaScript Assigment

---
+++ JavaScript Assigment/QUESTION 3/index.js
@@ -0,0 +1,9 @@
+//03. Declare a varibale and assign string value to it and change all the string characters to capital letters using toUpperCase() method.
+
+const str1= "Hello there !! This is Yash Arora here Currently i'm learning of FSJS batch 2.0 ";
+const strUpper=str1.toUpperCase();
+
+// Output This program
+
+console.log(strUpper);
+
