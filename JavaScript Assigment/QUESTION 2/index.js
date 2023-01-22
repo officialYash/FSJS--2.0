698ee55bdb279c0cc73b7f857a5fbddbc87bb049
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-22 16:27:33 +0530

    Question 2 done of JavaScript Assigment

---
+++ JavaScript Assigment/QUESTION 2/index.js
@@ -0,0 +1,13 @@
+//02. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+const firstName="Yash";
+const lastName="Arora"
+const maritalStatus="Unmarried";
+const country ="India";
+let age="25";
+
+const intro = `Hello !! My Name is ${firstName} ${lastName} and I am ${age} years old. I live in ${country} and Currently ${maritalStatus}`;
+
+// Output this Question
+
+console.log(intro);
