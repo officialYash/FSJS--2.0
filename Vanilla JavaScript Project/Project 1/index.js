75bcaaa7ea7a6ed983cf7599785e006853d1dfaa
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-23 14:18:24 +0530

    add eventListener in button

--- Vanilla JavaScript Project/Project 1/index.js
+++ Vanilla JavaScript Project/Project 1/index.js
@@ -2,7 +2,7 @@ let num = 1;
 
 const btn= document.createElement("button");
 btn.classList.add("btn");
-btn.innerText ="clean Screen";
+btn.innerText ="clear Screen";
 // console.log(btn);
 // console.log("clicked",circle);
 document.body.addEventListener("click" , (e)=>{
@@ -15,7 +15,12 @@ if( num === 1)
     circle.style.left=`${e.clientX}px`;
     circle.style.top=`${e.clientY}px`;
     circle.innerText=num;
+    circle.classList.add("circle");
     document.body.appendChild(circle);
-    console.log(e.clientX,e.clientY, num)
+    console.log(e.clientX, e.clientY, num)
     num++;
+})
+
+btn.addEventListener("mouseenter", ()=>{
+    location.reload();
 })
