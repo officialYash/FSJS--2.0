2819661b85ccad20994a6761f02f03ec7177e0a4
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-23 14:10:36 +0530

    added a clean screen button

--- Vanilla JavaScript Project/Project 1/index.js
+++ Vanilla JavaScript Project/Project 1/index.js
@@ -1,13 +1,21 @@
 let num = 1;
 
-document.body.addEventListener("click" ,(e)=>{
+const btn= document.createElement("button");
+btn.classList.add("btn");
+btn.innerText ="clean Screen";
+// console.log(btn);
+// console.log("clicked",circle);
+document.body.addEventListener("click" , (e)=>{
+if( num === 1)
+{
+    document.body.appendChild(btn);
+}
+
     const circle=document.createElement("div");
-    circle.classList.add("circle")
-    //console.log("clicked", circle);
     circle.style.left=`${e.clientX}px`;
     circle.style.top=`${e.clientY}px`;
     circle.innerText=num;
     document.body.appendChild(circle);
-    console.log(e.clientX,e.clientY,num)
+    console.log(e.clientX,e.clientY, num)
     num++;
 })
