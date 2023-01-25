c45fc3c318d9287dc15bb1a520cd503633e85011
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-25 18:54:34 +0530

    correction error css content

--- Vanilla JavaScript Project/Image Carosuel/index.js
+++ Vanilla JavaScript Project/Image Carosuel/index.js
@@ -1,27 +1,27 @@
-const slides = [...document.getElementsByClassName("slides")]
+const slides = [...document.getElementsByClassName("slide")]
 const btnNext = document.getElementsByClassName("next")[0];
 const btnPrev = document.getElementsByClassName("prev")[0];
-
 btnPrev.classList.add("btn_inactive")
 let n = 1;
-console.log(slides , btnNext ,btnPrev)
+console.log(slides, btnNext, btnPrev)
 
-// changing the position of images (images * 100%) from left
-slides.forEach((slide, index) => { slide.style.left = `${index * 100}%`})
+//changing position of images (index*100%) from left
+slides.forEach((slide, index) => {
+    slide.style.left = `${index * 100}%`
+})
 
-btnNext.addEventListener("click" , () =>{
-    if( n >= slides.length - 1){
+btnNext.addEventListener("click", () => {
+    if (n >= slides.length - 1) {
         btnNext.classList.add("btn_inactive")
     }
-    if( n >=slides.length){
-        n =slides.length;
-        console.log(n);
+    if (n >= slides.length) {
+        n = slides.length;
+        console.log(n)
 
-    }
-    else{
+    } else {
         btnPrev.classList.remove("btn_inactive")
-        slides.forEach((slide ,index) =>{
-        slide.style.left =`${(index - n ) * 100} %`
+        slides.forEach((slide, index) => {
+            slide.style.left = `${(index - n) * 100}%`;
         })
         n++;
         console.log(n);
@@ -29,18 +29,18 @@ btnNext.addEventListener("click" , () =>{
     }
 })
 
-btnPrev.addEventListener("click" , () => {
-    if( n <=1 ){
+btnPrev.addEventListener("click", () => {
+
+    if (n <= 1) {
         n = 1;
-    }
-    else{
-        if( n === 2){
+    } else {
+        if (n === 2) {
             btnPrev.classList.add("btn_inactive")
         }
         btnNext.classList.remove("btn_inactive")
         n--;
-        slides.forEach((slide ,index) =>{
-            slide.style.left = `${((index - n) + 1) * 100 }%`
+        slides.forEach((slide, index) => {
+            slide.style.left = `${((index - n) + 1) * 100}%`;
         })
         console.log(n);
         console.log("Prev Clicked")
