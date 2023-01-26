eea251917245d187eeab83035799b92e490db205
officialYash <38553961+officialYash@users.noreply.github.com>
2023-01-26 20:50:42 +0530

    Question 16 done of JavaScript Assignment

---
+++ JavaScript Assigment/QUESTION 16/index.js
@@ -0,0 +1,37 @@
+/*16. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+    - September, October or November, the season is Autumn.
+    - December, January or February, the season is Winter.
+    - March, April or May, the season is Spring
+    - June, July or August, the season is Summer */
+
+
+    let Inp = prompt("Enter the current month");
+    Inp=Inp.toLowerCase();
+
+    switch(Inp){
+        case "december":
+            case "january":
+                case "february":
+                    alert('Sesaon is is Winter🥶🤧. I need a coffee 🍵🍵');
+                        break;
+
+        case "september":
+            case "october":
+                case "november":
+                    alert(`Season is Autumn 🍂🍂`);
+                        break;
+        
+
+        case "june":
+            case "july":
+                case "august":
+                    alert(`Season is Summer 🌞☀️. Try some Icecream 🍦🍦 `);
+                        break;
+
+        case "march":
+            case "april":
+                case "may":
+                    alert(`Season is Spring 🌸💐. `);
+                        break;            
+
+                }
