02ccc5a8703a8ec68ee61e15223710131dac5541
officialYash <38553961+officialYash@users.noreply.github.com>
2023-02-09 22:40:31 +0530

    Question 6 Solved

---
+++ DOM PROJECT/06_DOM Project/06_DOM Project/index.js
@@ -0,0 +1,8 @@
+// Task-1
+const logo = document.querySelector("header > img");
+logo.src="./assets/ineuron-logo.png"
+
+//Task-2
+
+const appPrice = document.querySelector(".app_price > span");
+appPrice.innerText="$10";
