0dffbc16c73c8b8336ab1e9e07ad227129bffdd8
officialYash <38553961+officialYash@users.noreply.github.com>
2023-02-11 15:00:25 +0530

    Code Optimed

--- DOM PROJECT/DOM P7/DOM P7/index.js
+++ DOM PROJECT/DOM P7/DOM P7/index.js
@@ -1,9 +1,7 @@
 // task 1
 
 const languages = Array.from(document.querySelectorAll(".main__languages > a"))
-console.log(languages);
 languages.forEach(elem =>{
-    console.log(elem);
     if(elem.innerText.includes("2.0")){
         elem.style.display="none";
     }
@@ -15,3 +13,34 @@ const submitBtn = document.getElementsByClassName('main__form-btn')[0];
 submitBtn.disabled=false;
 const inputField = document.getElementsByClassName('main__form-input')[0];
 inputField.disabled=false;
+
+
+let namesValue;
+const language= document.querySelector(".main__languages");
+const names = localStorage.getItem('names');
+if(names){
+    namesValue=JSON.parse(names);
+}
+else
+{
+    namesValue=[];
+}
+submitBtn.addEventListener('click' , () =>{
+    const inputValue = inputField.value;
+    namesValue.push(inputValue);
+    localStorage.setItem('names' ,JSON.stringify(namesValue));
+})
+
+// function to add text in left card
+
+function addText() {
+    const aTag = language.getElementsByTagName('a')[0];
+    namesValue.forEach(text => {
+        const aTagNew = aTag.cloneNode(true);
+        aTagNew.innerText = text;
+        language.appendChild(aTagNew)
+    })
+}
+
+addText();
+
