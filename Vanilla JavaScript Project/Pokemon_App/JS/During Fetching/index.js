ff3eead38c9ea7a93359f339f1d41b6eeb2fae09
officialYash <38553961+officialYash@users.noreply.github.com>
2023-03-02 21:01:42 +0530

    During Fetching Javascript Update

---
+++ Vanilla JavaScript Project/Pokemon_App/JS/During Fetching/index.js
@@ -0,0 +1,23 @@
+import {
+    containerPokeCard,
+    prevBtn,  
+    loader,
+    pagination,
+    footer,
+  } from "../Constants Variables/index.js";
+
+  function fetchingStarts(){
+    loader.style.display ="inline-block";
+    pagination.style.display="none";
+    footer.style.display="none";
+  }
+
+  function fetchingEnds() {
+    loader.style.display = "none";
+    pagination.style.display = "flex";
+    footer.style.display = "flex";
+  }
+  
+  export{fetchingEnds,fetchingStarts}
+  
+  
