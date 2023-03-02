0c356823b809b1ac13f19371ac95ebd0269c4977
officialYash <38553961+officialYash@users.noreply.github.com>
2023-03-02 20:55:44 +0530

    Constants Variables  Javascript Update

--- Vanilla JavaScript Project/Pokemon_App/JS/Constants Variables/index.js
+++ Vanilla JavaScript Project/Pokemon_App/JS/Constants Variables/index.js
@@ -1,40 +1,22 @@
-const request = fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=40`);
-const containerPokeCard = document.getElementsByClassName("container")[0];
+let offset = 0;
+function modifyOffset(value){
+    return offset=value;
+}
 
-export { request, containerPokeCard};
+const containerPokeCard =document.getElementsByClassName("container")[0];
+const prevBtn = document.getElementById("prev");
+const nextBtn = document.getElementById("next");
+const loader = document.getElementsByClassName("lds-roller")[0];
+const pagination = document.getElementsById("pagination");
+const footer = document.getElementsByTagName("footer")[0];
 
-// request
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     const arr = data.results;
-//     const individualPokemonData = arr.map((x) => x.url);
-//     console.log(individualPokemonData);
-//     individualPokemonData.forEach((url) => {
-//       fetch(url)
-//         .then((response) => {
-//           return response.json();
-//         })
-//         .then((data) => {
-//           console.log(data);
-//           console.log(data.sprites.other.dream_world.front_default);
-//           containerPokeCard.innerHTML += ` <div class="card">
-//          <div class="card-header">
-//            <img
-//              src="${data.sprites.other.dream_world.front_default}"
-//              alt="pokemon"
-//            />
-//          </div>
-//          <div class="card-body">
-//            <h3>${data.name}</h3>
-//            <div class="type">
-//              <button class="button typeBtn" role="button">Grass</button>
-//              <button class="button readMoreBtn" role="button">Know More</button>
-//            </div>
-//          </div>
-//        </div>
-//          `;
-//         });
-//     });
-//   });
+export{
+    offset,
+  modifyOffset,
+  containerPokeCard,
+  prevBtn,
+  nextBtn,
+  loader,
+  pagination,
+  footer,
+};
